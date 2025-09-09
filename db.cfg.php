258e636286aae88f79cba5afbb5afa2d7a8f3388
Martin <martin@martinc.cz>
2025-09-09 13:21:17 +0200

    Změna konfigurace

--- db.cfg.php
+++ db.cfg.php
@@ -1,8 +1,8 @@
 <?php
 
-$server="localhost";  //umístění serveru s databází
-$database="tomas";  //jméno připojované databáze
-$login="tomas";  //login (uživatelské jméno) pro přístup k databázi
-$pass="n1k7ZHZRNj7EsfX9";  //heslo k databázi
+$server="";  //umístění serveru s databází
+$database="";  //jméno připojované databáze
+$login="";  //login (uživatelské jméno) pro přístup k databázi
+$pass="";  //heslo k databázi
 
 ?>
