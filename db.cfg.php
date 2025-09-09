2f39a9268ab98bdfb65f4486def1111ce1056547
Martin <martin@martinc.cz>
2025-09-09 13:19:00 +0200

    Přidán zápis do DB

---
+++ db.cfg.php
@@ -0,0 +1,8 @@
+<?php
+
+$server="localhost";  //umístění serveru s databází
+$database="tomas";  //jméno připojované databáze
+$login="tomas";  //login (uživatelské jméno) pro přístup k databázi
+$pass="n1k7ZHZRNj7EsfX9";  //heslo k databázi
+
+?>
---
+++ latrine.php
@@ -0,0 +1,139 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+include "db.cfg.php";
+
+###### Adjust the following storage location #########
+# Storage location for files with a trailing slash to indicate a directory
+# Currently it is set to latrine's dir
+	$store = './'; 
+//	$store = '/tmp/latrine/';
+
+###### Adjust housekeeping time (if not using /tmp)
+# Set to 24h. Change to fit your needs (time is in sec)
+#@$housekeeping = 60*24*60*60;
+###### Change the following only if you know what you're doing #########
+@$view = $_GET['view'];
+@$geo = $_GET['geo'];
+@$key = $_GET['key'];
+@$delete = $_GET['delete'];
+
+####### Locus Pro Parameters
+# Latitude - current GPS latitude (in degree unit, rounded on 5 decimal places)
+@$lat = $_GET['lat'];
+# Longitude - current GPS longitude (in degree unit, rounded on 5 decimal places)
+@$lon = $_GET['lon'];
+# Altitude - current altitude value in meter (improved altitude value by filter and barometer)
+@$alt = $_GET['alt'];
+# Speed - current GPS speed (in m/s)
+@$speed = $_GET['speed'];
+# Accuracy - current GPS accuracy (in metres)
+@$acc = $_GET['acc'];
+# Battery - current akku (in percent)
+@$battery = $_GET['battery'];
+# GSM signal - current signal strength (in percent)
+@$gsm_signal = $_GET['gsm_signal'];
+# Bearing - current GPS bearing (in degree)
+@$bearing = $_GET['bearing'];
+# Messag - variable Message you're able to provide with a selfdefined var in locus
+@$message = $_GET['message'];
+# Time - current GPS time (in format defined by user. You may choose from predefined styles or define you own by this specification)
+#my $time = param('time') || '';
+# Text field - text field with own define key/value pair.
+#y $var = param('var') || '';
+# Check storage location
+if (!is_dir($store)) {
+    if (!mkdir($store)) {
+        die("Úložiště není k dispozici");
+    }
+}
+
+@$spojeni=mysqli_Connect($server, $login, $pass, $database);
+if (!$spojeni){
+   echo "<center><font size=5>Chyba při připojování k serveru... <br><br><a href=javascript:history.back(-1)>Zpět</a>";
+}
+
+# Current Unix timestamp
+$time = time();
+$now = $time;
+# JSON output
+#echo header('application/json');
+header('Content-Type: application/json;charset=utf-8');
+echo '{';
+# Check private key
+if ($key!="") {
+        $geojson = $store.$key.".geojson";
+        $store .= "$key.latlon"; # Create storage location
+	# Housekeeping
+#	if (filemtime($store)< (time()-$housekeeping)) {
+#		unlink($geojson);
+#		unlink($store);
+#	}
+	##### Write to file
+        # Write latitude, longitude and other data into .latlon file when received from Locus
+        if ( $lat!="" && $lon!="" ) {
+        if ($handle = fopen($store, "w")) {
+            fwrite($handle,'"lat":"'.$lat.'","lon":"'.$lon.'","alt":"'.$alt.'","speed":"'.$speed.'","bearing":"'.$bearing.'","acc":"'.$acc.'","time":"'.$time.'","battery":"'.$battery.'","gsm_signal":"'.$gsm_signal.'","message":"'.$message.'"');
+            fclose($handle);
+            $zapis=mysqli_query($spojeni, "insert into Poloha values (NULL,'$lat','$lon','$alt','$speed','$bearing','$key','$acc','$battery','$gsm_signal');");
+        } else {
+            echo "Dateizugriffsfehler .latlon";
+        }
+	# Write latitude & longitude into .geojson file, collecting positions for track
+        if ($handle = fopen($geojson, "a+")) {
+                fwrite($handle,"[ ".$lon.", ".$lat." ], ");
+                fclose($handle);
+        } else {
+                echo "Dateizugriffsfehler .geojson";
+        }
+	##### Read from file
+        # Output latitude, longitude and other data as JSON
+        } else if ($view == '1' ) {
+                if ($handle = fopen($store, "r")) {
+                    $contents=fread($handle,filesize($store));
+                    $contents=$contents.',"now":"'.$now.'"';
+                    echo $contents;
+                    fclose($handle);
+                } else {
+                        echo 'Dateilesefahler view=1';
+                }
+        # Output latitude, longitude as JSON for drawing track
+        } else if ($geo == '1') {
+                if ($handle = fopen($geojson, "r")) {
+                    $contents=fread($handle,filesize($geojson));
+                    //Get rid of last ","
+                    $contents=substr($contents,0,strrpos($contents,","));
+                    $contents=' "type": "FeatureCollection",
+                           "features": [
+                             { "type": "Feature",
+                               "geometry": {
+                         "type": "MultiLineString",
+                         "coordinates": [[ 
+                        '.$contents.'
+                        ]]
+                        }
+                        }
+                        ]
+                        ';
+                    echo $contents;
+                    fclose($handle);
+                } else {
+                        echo 'Dateilesefehler geo=1';
+                }
+	# Delete .latlon & .geojson by purpose
+#	} else if ($delete == '1') {
+#		unlink($geojson);
+#		unlink($store);
+        # Parameters are missing
+        } else {
+                echo '"error":"bad parameters"';
+        }
+# Private key is missing
+} else {
+    echo 'Schlüssel falsch oder fehlt';
+}
+# Done!
+echo '}';
+mysqli_close($spojeni);
+?>
